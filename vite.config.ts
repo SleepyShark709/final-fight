3942dcab4802175884c8056d4dc63abf0deddbf0
shark <695622645@qq.com>
2025-04-19 23:59:10 +0800

    准备部署到 GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import { vue } from "@vitejs/plugin-vue";
 
 export default defineConfig({
   base: "/final-fight/",
-  plugins: [vue()],
   server: {
     port: 3000,
     open: true,
