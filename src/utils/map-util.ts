88ccd8637ab7d8284fff87f5a7594de34c76213d
shark <695622645@qq.com>
2025-04-20 23:15:26 +0800

    feat: 重构图片引用方式

--- src/utils/map-util.ts
+++ src/utils/map-util.ts
@@ -1,18 +1,30 @@
+import t1 from "@/assets/grass/t1.png";
+import t2 from "@/assets/grass/t2.png";
+import t3 from "@/assets/grass/t3.png";
+import t4 from "@/assets/grass/t4.png";
+import t5 from "@/assets/grass/t5.png";
+import t6 from "@/assets/grass/t6.png";
+import t7 from "@/assets/grass/t7.png";
+import t8 from "@/assets/grass/t8.png";
+import t9 from "@/assets/grass/t9.png";
+import t10 from "@/assets/grass/t10.png";
+import t11 from "@/assets/grass/t11.png";
+
 export class MapImages {
   images: { [key: string]: string };
   constructor() {
     this.images = {
-      t1: "image/map/grass/t1.png",
-      t2: "image/map/grass/t2.png",
-      t3: "image/map/grass/t3.png",
-      t4: "image/map/grass/t4.png",
-      t5: "image/map/grass/t5.png",
-      t6: "image/map/grass/t6.png",
-      t7: "image/map/grass/t7.png",
-      t8: "image/map/grass/t8.png",
-      t9: "image/map/grass/t9.png",
-      t10: "image/map/grass/t10.png",
-      t11: "image/map/grass/t11.png",
+      t1: t1,
+      t2: t2,
+      t3: t3,
+      t4: t4,
+      t5: t5,
+      t6: t6,
+      t7: t7,
+      t8: t8,
+      t9: t9,
+      t10: t10,
+      t11: t11,
     };
   }
 }
