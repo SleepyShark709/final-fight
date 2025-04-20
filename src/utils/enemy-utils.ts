aee57ef6f881d27acedf9081be95552abddd99aa
shark <695622645@qq.com>
2025-04-20 23:15:26 +0800

    feat: 重构图片引用方式

--- src/utils/enemy-utils.ts
+++ src/utils/enemy-utils.ts
@@ -1,41 +1,67 @@
+import eidle0 from "@/assets/enemy/idle/0.png";
+import eidle1 from "@/assets/enemy/idle/1.png";
+import eidle2 from "@/assets/enemy/idle/2.png";
+import eidle3 from "@/assets/enemy/idle/3.png";
+
+import edie0 from "@/assets/enemy/die/0.png";
+import edie1 from "@/assets/enemy/die/1.png";
+import edie2 from "@/assets/enemy/die/2.png";
+import edie3 from "@/assets/enemy/die/3.png";
+
+import ewalk0 from "@/assets/enemy/walk/0.png";
+import ewalk1 from "@/assets/enemy/walk/1.png";
+import ewalk2 from "@/assets/enemy/walk/2.png";
+import ewalk3 from "@/assets/enemy/walk/3.png";
+import ewalk4 from "@/assets/enemy/walk/4.png";
+import ewalk5 from "@/assets/enemy/walk/5.png";
+
+import eattack0 from "@/assets/enemy/attack/0.png";
+import eattack1 from "@/assets/enemy/attack/1.png";
+import eattack2 from "@/assets/enemy/attack/2.png";
+import eattack3 from "@/assets/enemy/attack/3.png";
+import eattack4 from "@/assets/enemy/attack/4.png";
+import eattack5 from "@/assets/enemy/attack/5.png";
+import eattack6 from "@/assets/enemy/attack/6.png";
+import eattack7 from "@/assets/enemy/attack/7.png";
+
 export class EnemyImages {
   images: { [key: string]: string };
   constructor() {
     this.images = {
-      eidle0: "image/enemy/idle/0.png",
-      eidle1: "image/enemy/idle/1.png",
-      eidle2: "image/enemy/idle/2.png",
-      eidle3: "image/enemy/idle/3.png",
+      eidle0: eidle0,
+      eidle1: eidle1,
+      eidle2: eidle2,
+      eidle3: eidle3,
       // eidle4: 'image/enemy/idle/4.png',
       // eidle5: 'image/enemy/idle/5.png',
       // eidle6: 'image/enemy/idle/6.png',
       // eidle7: 'image/enemy/idle/7.png',
-      edie0: "image/enemy/die/0.png",
-      edie1: "image/enemy/die/1.png",
-      edie2: "image/enemy/die/2.png",
-      edie3: "image/enemy/die/3.png",
+      edie0: edie0,
+      edie1: edie1,
+      edie2: edie2,
+      edie3: edie3,
       // edie4: 'image/enemy/die/4.png',
       // edie5: 'image/enemy/die/5.png',
       // edie6: 'image/enemy/die/6.png',
       // edie7: 'image/enemy/die/7.png',
       // edie8: 'image/enemy/die/8.png',
       // edie9: 'image/enemy/die/9.png',
-      ewalk0: "image/enemy/walk/0.png",
-      ewalk1: "image/enemy/walk/1.png",
-      ewalk2: "image/enemy/walk/2.png",
-      ewalk3: "image/enemy/walk/3.png",
-      ewalk4: "image/enemy/walk/4.png",
-      ewalk5: "image/enemy/walk/5.png",
+      ewalk0: ewalk0,
+      ewalk1: ewalk1,
+      ewalk2: ewalk2,
+      ewalk3: ewalk3,
+      ewalk4: ewalk4,
+      ewalk5: ewalk5,
       // ewalk6: 'image/enemy/walk/6.png',
       // ewalk7: 'image/enemy/walk/7.png',
-      eattack0: "image/enemy/attack/0.png",
-      eattack1: "image/enemy/attack/1.png",
-      eattack2: "image/enemy/attack/2.png",
-      eattack3: "image/enemy/attack/3.png",
-      eattack4: "image/enemy/attack/4.png",
-      eattack5: "image/enemy/attack/5.png",
-      eattack6: "image/enemy/attack/6.png",
-      eattack7: "image/enemy/attack/7.png",
+      eattack0: eattack0,
+      eattack1: eattack1,
+      eattack2: eattack2,
+      eattack3: eattack3,
+      eattack4: eattack4,
+      eattack5: eattack5,
+      eattack6: eattack6,
+      eattack7: eattack7,
       // eattack8: 'image/enemy/attack/8.png',
       // eattack9: 'image/enemy/attack/9.png',
     };
--- src/utils/player-util.ts
+++ src/utils/player-util.ts
@@ -1,41 +1,79 @@
+import run0 from "@/assets/player/run/0.png";
+import run1 from "@/assets/player/run/1.png";
+import run2 from "@/assets/player/run/2.png";
+import run3 from "@/assets/player/run/3.png";
+import run4 from "@/assets/player/run/4.png";
+import run5 from "@/assets/player/run/5.png";
+
+import idle0 from "@/assets/player/idle-sword/0.png";
+import idle1 from "@/assets/player/idle-sword/1.png";
+import idle2 from "@/assets/player/idle-sword/2.png";
+import idle3 from "@/assets/player/idle-sword/3.png";
+import idle4 from "@/assets/player/idle-sword/4.png";
+import idle5 from "@/assets/player/idle-sword/5.png";
+
+import attack1_0 from "@/assets/player/attack/1-0.png";
+import attack1_1 from "@/assets/player/attack/1-1.png";
+import attack1_2 from "@/assets/player/attack/1-2.png";
+import attack1_3 from "@/assets/player/attack/1-3.png";
+import attack1_4 from "@/assets/player/attack/1-4.png";
+import attack1_5 from "@/assets/player/attack/1-5.png";
+
+import attack2_0 from "@/assets/player/attack/2-0.png";
+import attack2_1 from "@/assets/player/attack/2-1.png";
+import attack2_2 from "@/assets/player/attack/2-2.png";
+import attack2_3 from "@/assets/player/attack/2-3.png";
+import attack2_4 from "@/assets/player/attack/2-4.png";
+import attack2_5 from "@/assets/player/attack/2-5.png";
+
+import attack3_0 from "@/assets/player/attack/3-0.png";
+import attack3_1 from "@/assets/player/attack/3-1.png";
+import attack3_2 from "@/assets/player/attack/3-2.png";
+import attack3_3 from "@/assets/player/attack/3-3.png";
+
+import jump0 from "@/assets/player/jump/0.png";
+import jump1 from "@/assets/player/jump/1.png";
+import jump2 from "@/assets/player/jump/2.png";
+import jump3 from "@/assets/player/jump/3.png";
+
 export class PlayerImages {
   images: { [key: string]: string };
   constructor() {
     this.images = {
-      run0: "image/player/run/0.png",
-      run1: "image/player/run/1.png",
-      run2: "image/player/run/2.png",
-      run3: "image/player/run/3.png",
-      run4: "image/player/run/4.png",
-      run5: "image/player/run/5.png",
-      idle0: "image/player/idle-sword/0.png",
-      idle1: "image/player/idle-sword/1.png",
-      idle2: "image/player/idle-sword/2.png",
-      idle3: "image/player/idle-sword/3.png",
-      idle4: "image/player/idle-sword/4.png",
-      idle5: "image/player/idle-sword/5.png",
-      attack1_0: "image/player/attack/1-0.png",
-      attack1_1: "image/player/attack/1-1.png",
-      attack1_2: "image/player/attack/1-2.png",
-      attack1_3: "image/player/attack/1-3.png",
-      attack1_4: "image/player/attack/1-4.png",
-      attack1_5: "image/player/attack/1-5.png",
-      attack2_0: "image/player/attack/2-0.png",
-      attack2_1: "image/player/attack/2-1.png",
-      attack2_2: "image/player/attack/2-2.png",
-      attack2_3: "image/player/attack/2-3.png",
-      attack2_4: "image/player/attack/2-4.png",
-      attack2_5: "image/player/attack/2-5.png",
-      attack3_0: "image/player/attack/3-0.png",
-      attack3_1: "image/player/attack/3-1.png",
-      attack3_2: "image/player/attack/3-2.png",
-      attack3_3: "image/player/attack/3-3.png",
+      run0: run0,
+      run1: run1,
+      run2: run2,
+      run3: run3,
+      run4: run4,
+      run5: run5,
+      idle0: idle0,
+      idle1: idle1,
+      idle2: idle2,
+      idle3: idle3,
+      idle4: idle4,
+      idle5: idle5,
+      attack1_0: attack1_0,
+      attack1_1: attack1_1,
+      attack1_2: attack1_2,
+      attack1_3: attack1_3,
+      attack1_4: attack1_4,
+      attack1_5: attack1_5,
+      attack2_0: attack2_0,
+      attack2_1: attack2_1,
+      attack2_2: attack2_2,
+      attack2_3: attack2_3,
+      attack2_4: attack2_4,
+      attack2_5: attack2_5,
+      attack3_0: attack3_0,
+      attack3_1: attack3_1,
+      attack3_2: attack3_2,
+      attack3_3: attack3_3,
       // attack3_4: 'image/attack/3-4.png',
       // attack3_5: 'image/attack/3-5.png',
-      jump0: "image/player/jump/0.png",
-      jump1: "image/player/jump/1.png",
-      jump2: "image/player/jump/2.png",
-      jump3: "image/player/jump/3.png",
+      jump0: jump0,
+      jump1: jump1,
+      jump2: jump2,
+      jump3: jump3,
     };
   }
 }
